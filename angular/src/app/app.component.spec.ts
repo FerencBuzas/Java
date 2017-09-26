f83cbc356a3ec3175f11fe8ece0c088445c66acc
Ferenc Buzas <spapi99@gmail.com>
2017-09-26 13:00:28 +0200

    Commented out some test cases, to have just one running.

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -4,29 +4,35 @@ import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
+    console.info("\n\nbeforeEach ##");
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+    }).overrideComponent(AppComponent, {
+        set: { styleUrls: [] }
     }).compileComponents();
   }));
 
   it('should create the app', async(() => {
+    console.info("\n\nit(should create the app) ##") ;
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
+//  it(`should have as title 'Sheet Music'`, async(() => {
+//    console.info("\n\nit(should have a title) ##");
+//    const fixture = TestBed.createComponent(AppComponent);
+//    const app = fixture.debugElement.componentInstance;
+//    expect(app.title).toEqual('Sheet Music');
+//  }));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+//  it('should render title in a h1 tag', async(() => {
+//    console.info("\n\nit(should render a title) ##");
+//    const fixture = TestBed.createComponent(AppComponent);
+//    fixture.detectChanges();
+//    const compiled = fixture.debugElement.nativeElement;
+//    expect(compiled.querySelector('h1').textContent).toContain('Sheet Music');
   }));
 });
