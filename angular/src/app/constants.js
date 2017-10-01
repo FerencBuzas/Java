1c4648f7e9879be52d7270ab750643a4854e7cad
Ferenc Buzas <spapi99@gmail.com>
2017-10-01 19:05:10 +0200

    Removed unnecessary files.

--- angular/src/app/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Constants = (function () {
-    function Constants() {
-    }
-    Constants.URL_BASE = "http://localhost:8080/music";
-    return Constants;
-}());
-exports.Constants = Constants;
-//# sourceMappingURL=constants.js.map
