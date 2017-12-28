5de27e737bbe89d4fe64c6d62c9f02f866adb44f
Ferenc Buzas <spapi99@gmail.com>
2017-12-28 20:43:17 +0100

    Improved creating test data; renamed test methods to start with 'test'.

--- service/src/main/java/music/app/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package music.app;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "music.common, music.dao")
-public class AppConfig {
-}
